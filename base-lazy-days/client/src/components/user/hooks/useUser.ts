05a06a986d9e5dbed114f7a504debd3170eb1c12
jaeyoung <wooder2050@gmail.com>
2022-04-18 16:38:09 +0900

    complete : session 7 - React Query and Authentication

--- base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -1,4 +1,5 @@
 import { AxiosResponse } from 'axios';
+import { useQuery, useQueryClient } from 'react-query';
 
 import type { User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
@@ -9,16 +10,19 @@ import {
   setStoredUser,
 } from '../../../user-storage';
 
-// async function getUser(user: User | null): Promise<User | null> {
-//   if (!user) return null;
-//   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
-//     `/user/${user.id}`,
-//     {
-//       headers: getJWTHeader(user),
-//     },
-//   );
-//   return data.user;
-// }
+async function getUser(
+  user: User | null,
+  signal: AbortSignal,
+): Promise<User | null> {
+  if (!user) return null;
+  const {
+    data,
+  }: AxiosResponse<{ user: User }> = await axiosInstance.get(
+    `/user/${user.id}`,
+    { signal, headers: getJWTHeader(user) },
+  );
+  return data.user;
+}
 
 interface UseUser {
   user: User | null;
@@ -27,17 +31,48 @@ interface UseUser {
 }
 
 export function useUser(): UseUser {
-  // TODO: call useQuery to update user data from server
-  const user = null;
+  const queryClient = useQueryClient();
+  const { data: user } = useQuery<User>(
+    queryKeys.user,
+    ({ signal }) => getUser(user, signal),
+    // ALTERNATE query function to maintain user after mutation
+    // (see https://www.udemy.com/course/learn-react-query/learn/#questions/17098438/
+    // for discussion)
+    // ({ signal }) => {
+    //   const storedUser = getStoredUser();
+    //   const currentUser = user ?? storedUser;
+    //   return getUser(currentUser, signal);
+    // },
+    {
+      // populate initially with user in localStorage
+      initialData: getStoredUser,
+
+      // note: onSuccess is called on both successful query function completion
+      //     *and* on queryClient.setQueryData
+      // the `received` argument to onSuccess will be:
+      //    - null, if this is called on queryClient.setQueryData in clearUser()
+      //    - User, if this is called from queryClient.setQueryData in updateUser()
+      //         *or* from the getUser query function call
+      onSuccess: (received: User | null) => {
+        if (!received) clearStoredUser();
+        else setStoredUser(received);
+      },
+    },
+  );
 
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
-    // TODO: update the user in the query cache
+    // update the user
+    queryClient.setQueryData(queryKeys.user, newUser);
   }
 
   // meant to be called from useAuth
   function clearUser() {
-    // TODO: reset user to null in query cache
+    // reset user to null
+    queryClient.setQueryData(queryKeys.user, null);
+
+    // remove user appointments query
+    queryClient.removeQueries([queryKeys.appointments, queryKeys.user]);
   }
 
   return { user, updateUser, clearUser };
